7bc055e2598729064dbc2842e36192dd94afda65
matuumdq <salinasmateo@outlook.com>
2022-07-27 13:06:52 -0300

    cambiando if en funcion por switch

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,20 +1,21 @@
-let seleccion = ""
-let precio = 0
-
+// let seleccion = ""
+// let precio = 0
 
 function seleccionPlan(){
     seleccion=prompt('Selecciona un numero para el Plan deseado: 1- Simple, 2- Famliar, 3- Premium')
-if(seleccion==1) {
+switch(seleccion) {
+        case "1": 
         precio = 400
         totIva(precio)
         console.log ("Solicitara el Plan Simple por: $", total, "con iva e impuestos incluidos")
-        cantidad = parseInt(prompt("Cuantas personas lo usaran?(maximo 2"))
-        if (cantidad<=2)
-        {personas(cantidad)
+        cantidad = parseInt(prompt("Cuantas personas lo usaran?(maximo 2)"))
+        if (cantidad<=2) {
+            personas(cantidad)
         } else {
             console.log("Numero de usuarios no permitido")
-        }
-} else if(seleccion==2) {
+        } 
+        break;
+        case "2":
         precio = 800
         totIva(precio)
         console.log ("Solicitara el Plan Familiar por: $", total, "con iva e impuestos incluidos")
@@ -24,7 +25,8 @@ if(seleccion==1) {
         } else {
             console.log("Numero de usuarios no permitido")
         }
-} else if(seleccion==3) { 
+        break
+        case "3" :
         precio = 1200
         totIva(precio)
         console.log ("Solicitara el Plan Premium por: $", total, "con iva e impuestos incluidos")
@@ -34,19 +36,18 @@ if(seleccion==1) {
         } else {
             console.log("Numero de usuarios no permitido")
         }
-    } 
-        else {
+        break 
+        default : 
         console.log('Seleccione una opcion valida')
-        personas()
+        seleccionPlan()
+    }}
+
+    function totIva(valor){
+        iva = (valor*1.21)
+        total = (iva * 1.50)
     }
-}
-
-function totIva(valor){
-    iva = (valor*1.21)
-    total = (iva * 1.50)
-}
-
-function personas(numPersonas) {
-    for (i=1; i<=numPersonas; i++)
-    console.log("Hola Persona: ", i)
-}
+    
+    function personas(numPersonas) {
+        for (i=1; i<=numPersonas; i++)
+        console.log("Hola Persona: ", i)
+    }
