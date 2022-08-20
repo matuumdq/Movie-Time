7c3af66c53cc361a023015941253570a5c5bc203
matuumdq <salinasmateo@outlook.com>
2022-08-20 16:29:01 -0300

    nuevo HTML, cambio en funciones, agregar a lista y quitar de lista

---
+++ js/bdd.js
@@ -0,0 +1,135 @@
+const peliculas = [
+    {
+        nombre: 'Wonder Woman',
+        genero: 'accion',
+        calificacion: 7.4,
+        imagen: 'https://dam.smashmexico.com.mx/wp-content/uploads/2019/12/Poster1.jpg',
+        id:1
+    }, 
+    {
+        nombre: 'Justice League',
+        genero: 'accion',
+        calificacion: 6.1,
+        imagen: "https://dam.smashmexico.com.mx/wp-content/uploads/2019/12/Poster2-691x1024.jpg",
+        id:2
+    }, 
+    {
+        nombre: 'The Dark Knight Rises',
+        genero: 'accion',
+        calificacion: 8.4,
+        imagen: "https://dam.smashmexico.com.mx/wp-content/uploads/2019/12/Poster3.jpg",
+        id: 3
+    }, 
+    {
+        nombre: 'The Joker',
+        genero: 'accion',
+        calificacion: 8.4,
+        imagen: "https://dam.smashmexico.com.mx/wp-content/uploads/2019/12/Poster4.jpg",
+        id: 4
+    }, 
+    {
+        nombre: 'Batman v Superman: Dawn Of Justice',
+        genero: 'accion',
+        calificacion: 6.4,
+        imagen: "https://dam.smashmexico.com.mx/wp-content/uploads/2019/12/Poster5.jpg",
+        id: 5
+    }, 
+    {
+        nombre: 'Inception',
+        genero: 'accion',
+        calificacion: 8.8,
+        imagen: "https://www.themoviedb.org/t/p/w220_and_h330_face/tXQvtRWfkUUnWJAn2tN3jERIUG.jpg",
+        id: 6
+    }, 
+    {
+        nombre: 'Mad Max',
+        genero: 'accion',
+        calificacion: 8.1,
+        imagen: "https://cdn.oldskull.net/wp-content/uploads/2015/07/Flore-Maquin-movie-posters-illustration-mad-max-.jpg",
+        id: 7
+    }, 
+    {
+        nombre: 'The amazing SpiderMan',
+        genero: 'accion',
+        calificacion: 6.9,
+        imagen: "https://http2.mlstatic.com/D_NQ_NP_729934-MLA42963890145_072020-O.webp",
+        id: 8
+    }, 
+    {
+        nombre: 'Fight Club',
+        genero: 'drama',
+        calificacion: 8.8,
+        imagen: "https://cdn.oldskull.net/wp-content/uploads/2015/07/Flore-Maquin-movie-posters-illustration-fight-club-.jpg",
+        id: 9
+    }, 
+    {
+        nombre: 'Django',
+        genero: 'drama',
+        calificacion: 8.4,
+        imagen: "https://cdn.oldskull.net/wp-content/uploads/2015/07/Flore-Maquin-movie-posters-illustration-django-.jpg",
+        id: 10
+    }, 
+    {
+        nombre: 'Interstellar',
+        genero: 'drama',
+        calificacion: 8.6,
+        imagen: "https://cdn.oldskull.net/wp-content/uploads/2015/07/Flore-Maquin-movie-posters-illustration-interstellar-.jpg",
+        id: 11
+    }, 
+    {
+        nombre: 'Cisne Negro',
+        genero: 'drama',
+        calificacion: 8.0,
+        imagen: "https://hips.hearstapps.com/es.h-cdn.co/fotoes/images/media/imagenes/reportajes/los-20-posters-de-peliculas-mas-creativos/cisne-negro/7055592-1-esl-ES/CISNE-NEGRO.jpg?resize=980:*",
+        id: 12
+    }, 
+    {
+        nombre: 'Pulp Fiction',
+        genero: 'crimen',
+        calificacion: 8.9,
+        imagen: "https://cdn.oldskull.net/wp-content/uploads/2015/07/Flore-Maquin-movie-posters-illustration-pulp-fiction-.jpg",
+        id: 13
+    }, 
+    {
+        nombre: 'The Mask',
+        genero: 'comedia',
+        calificacion: 6.9,
+        imagen: "https://cdn.oldskull.net/wp-content/uploads/2015/07/Flore-Maquin-movie-posters-illustration-the-mask-.jpg",
+        id: 14
+    }, 
+    {
+        nombre: 'Men in Black',
+        genero: 'comedia',
+        calificacion: 7.3,
+        imagen: "https://hips.hearstapps.com/es.h-cdn.co/fotoes/images/media/imagenes/reportajes/los-20-posters-de-peliculas-mas-creativos/men-in-black-3/7055676-1-esl-ES/MEN-IN-BLACK-3.jpg?resize=980:*",
+        id: 15
+    }, 
+    {
+        nombre: 'The Truman Show',
+        genero: 'comedia',
+        calificacion: 8.2,
+        imagen: "https://hips.hearstapps.com/es.h-cdn.co/fotoes/images/media/imagenes/reportajes/los-20-posters-de-peliculas-mas-creativos/show-truman/7055694-1-esl-ES/SHOW-TRUMAN.jpg?resize=980:*",
+        id: 16
+    }, 
+    {
+        nombre: 'Brave',
+        genero: 'infantiles',
+        calificacion: 7.1,
+        imagen: "https://i0.wp.com/elpoderdelasideas.com/wp-content/uploads/posters-cine-2012-5.jpg?resize=550%2C814",
+        id: 17
+    }, 
+    {
+        nombre: 'Frankenweenie',
+        genero: 'comedia',
+        calificacion: 6.9,
+        imagen: "https://i0.wp.com/elpoderdelasideas.com/wp-content/uploads/posters-cine-2012-10.jpg?resize=550%2C786",
+        id: 18
+    }, 
+    {
+        nombre: 'Hotel Transylvania',
+        genero: 'comedia',
+        calificacion: 7.0,
+        imagen: "https://i0.wp.com/elpoderdelasideas.com/wp-content/uploads/posters-cine-2012-11.jpg",
+        id: 19
+    }, 
+]
--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,179 +1,109 @@
-const peliculas = []
-const series = ['BREAKING BAD', 'THE BOYS', 'MR. ROBOT', 'PEAKY BLINDERS', 'THE SINNER']
-const seccionPeli = document.getElementById('card-pelis')
+let pelisLista = JSON.parse(localStorage.getItem("PelisenLista")) || []
 
-const nombreBuscarPeli = document.querySelector('#peli-buscada')
-const btnBuscarPeli = document.querySelector('#buscar')
+const btnAgregarLista = document.querySelector('#listaPeli')
+const contenedorLista = document.querySelector('#listado')
+const contenedorListado = document.querySelector('#listado .pelis')
 
-const inpNombre = document.querySelector('#agregar-nombre')
-const inpGenero = document.querySelector('#agregar-genero')
-const inpCalif = document.querySelector('#agregar-calif')
-const btnAgregarPeli = document.querySelector('#agregar-peli')
+const containerDiv = document.querySelector(".container");
 
-const textoBusqueda = document.querySelector('.formBuscarPeli')
 
-function seleccionPlan(){
-    seleccion=prompt('Selecciona un numero para el Plan deseado: 1- Simple, 2- Famliar, 3- Premium')
-    switch(seleccion) {
-        case "1": 
-            precio = 400
-            totIva(precio)
-            console.log ("Solicitara el Plan Simple por: $", total, "con iva e impuestos incluidos")
-            cantidad = parseInt(prompt("Cuantas personas lo usaran?(maximo 2)"))
-                if (cantidad<=2) {
-                    personas(cantidad)
-                } else {
-                    console.log("Numero de usuarios no permitido")
-                } 
-        break;
-        case "2":
-            precio = 800
-            totIva(precio)
-            console.log ("Solicitara el Plan Familiar por: $", total, "con iva e impuestos incluidos")
-            cantidad = parseInt(prompt("Cuantas personas lo usaran?(maximo 4)"))
-                if (cantidad<=4){
-                personas(cantidad)
-                } else {
-                    console.log("Numero de usuarios no permitido")
-                }
-            break
-        case "3" :
-            precio = 1200
-            totIva(precio)
-            console.log ("Solicitara el Plan Premium por: $", total, "con iva e impuestos incluidos")
-            cantidad = parseInt(prompt("Cuantas personas lo usaran?(maximo 4)"))
-                if (cantidad<=4){
-                    personas(cantidad)
-                } else {
-                    console.log("Numero de usuarios no permitido")
-                }      
-        break 
-        default : 
-            console.log('Seleccione una opcion valida')
-            seleccionPlan()
+
+btnAgregarLista.addEventListener('click', agregarPeli);
+
+
+
+function crearPelis(){
+    peliculas.forEach((pelis) => {
+        containerDiv.innerHTML += `<div class="card" style="width: 18rem">
+        <img
+          class="card-img-top"
+          src="${pelis.imagen}"
+          alt="${pelis.nombre}"
+        />
+        <div class="card-body">
+          <p class="card-title">${pelis.nombre}</p>
+          <a type="button" class="btn btn-primary agregar-lista" data-id="${pelis.id}">Agregar a la Lista</a>
+        </div>
+      </div>`
+    })
+}
+
+function miListaHTML(){
+
+    //limpiar el HTML
+    limpiarHTML();
+    //recorre pelis y genera HTML
+    pelisLista.forEach( peli => {
+        const { imagen, nombre, id} = peli;
+        const row = document.createElement('div')
+        row.classList.add('card')
+        row.innerHTML += `
+                <img class="card-img-top" src="${imagen}" alt=""/>
+            <div class="card-body">
+            <p class="card-title">${nombre}</p>
+            <a type="button" class="btn btn-primary quitar-lista" id="${id}">Quitar de la Lista</a>
+            </div>
+            `;
+
+            //Agregar Pelicula a la lista
+            contenedorLista.appendChild(row);
+            localStorage.setItem("PelisenLista", JSON.stringify(pelisLista));
+    })
+}
+
+
+function agregarPeli(e){
+    e.preventDefault();
+
+    if (e.target.classList.contains('agregar-lista')){
+        e.target.classList.add('disabled')
+        e.target.textContent = 'Ya esta en la lista'
+        const peliSeleccionada = e.target.parentElement.parentElement;
+        leerDatosPeli(peliSeleccionada);
+    }
+    if (e.target.classList.contains('quitar-lista')){
+        const peliSeleccionada = e.target.parentElement.parentElement;
+        borrarPeli(peliSeleccionada);
+    }
+
+}
+
+
+function leerDatosPeli(peli){
+
+    const infoPeli = {
+        imagen: peli.querySelector('img').src, 
+        nombre: peli.querySelector('p').textContent,
+        id: peli.querySelector('a').getAttribute('data-id'),
+    }
+    const existe = pelisLista.some(peli => peli.id === infoPeli.id);
+    if (existe){
+        return
+    } else {
+        pelisLista = [...pelisLista, infoPeli]
+        miListaHTML();
+}
+}
+
+function borrarPeli(peli){
+    const infoPeli = {
+        imagen: peli.querySelector('img').src, 
+        nombre: peli.querySelector('p').textContent,
+        id: peli.querySelector('a').getAttribute('id'),
+    }
+
+    const existe = pelisLista.some(peli => peli.id === infoPeli.id);
+    if (existe){
+        pelisLista = pelisLista.filter((pelisFilter) => pelisFilter.id !== infoPeli.id)
+   miListaHTML()
     }}
 
-    function totIva(valor){
-        iva = (valor*1.21)
-        total = (iva * 1.50)
+function limpiarHTML() {
+    while(contenedorLista.firstChild){
+        contenedorLista.removeChild(contenedorLista.firstChild)
     }
     
-    function personas(numPersonas) {
-        for (i=1; i<=numPersonas; i++)
-        console.log("Hola Persona: ", i)
-    }
+}
 
-
-    // Peliculas
-    class Pelicula {
-        constructor(nombre, genero, calificacion) {
-            this.nombre = nombre
-            this.genero = genero
-            this.calificacion = calificacion
-        }
-    }
-
-    function generadorPelis() {
-        peliculas.push(new Pelicula('inception', 'accion', 8.8))
-        peliculas.push(new Pelicula('fracture', 'crimen', 7.2))
-        peliculas.push(new Pelicula('perdida', 'suspenso', 7.3))
-        peliculas.push(new Pelicula('el club de la pelea', 'suspenso', 8.8))
-        peliculas.push(new Pelicula('hogar', 'drama', 6.4))
-        peliculas.push(new Pelicula('el bar', 'aventura', 6.3))
-    }
-
-    generadorPelis()
-
-    function agregarPeli(nombrePeli, generoPeli, calificacionPeli){
-      
-            let nombre = nombrePeli
-            let genero = generoPeli
-            let calificacion = parseFloat(calificacionPeli).toFixed(1)
-            peliculas.push(new Pelicula(nombre, genero, calificacion))
-            limpiarHTMLPeli()
-            const cardPelicula = document.createElement('div')
-            cardPelicula.className="pelis"
-            peliculas.forEach(peli => {
-                cardPelicula.innerHTML += `<div class="card pelicula">
-                <div class="card-body">
-                  <h5 class="card-title">${peli.nombre}</h5>
-                  <p class="card-text">${peli.genero}</p>
-                  <p class="card-text">Calificacion: ${peli.calificacion}</p>
-                  <a href="#" class="btn btn-primary">Ver</a>
-                  <a href="#" class="btn btn-primary">Descargar</a>
-                </div>
-              </div>`
-              seccionPeli.appendChild(cardPelicula)
-            })
-    }
-
-    function peliDisponible(nombre){
-        let nombrePeli = nombre.toLowerCase()
-        const existe = peliculas.some(peli=> peli.nombre === (nombrePeli))
-        const textBusqueda = document.querySelector('#textoDisponible')
-        textBusqueda.className="parrafoPeli"
-        textBusqueda.innerHTML = ('La pelicula esta disponible: ' + existe)
-        textoBusqueda.appendChild(textBusqueda)
-    }
-    
-    function busquePeliGenero(){
-      let generoABuscar = prompt('Ingrese genero de la peli que quiera ver: ').toLowerCase()
-      const peliPorGener = peliculas.filter(pelis => pelis.genero.includes(generoABuscar))
-      console.log('Puede ver la peli: ', peliPorGener)
-    }
-
-    function ordenarPelis(){
-        peliculas.sort((a,b) => {
-            if (a.calificacion < b.calificacion){
-              return 1
-            }
-            if (a.calificacion > b.calificacion){
-              return -1
-            }
-            return 0
-        })
-        console.table(peliculas)
-    }
-
-    
-        // Series
-
-    function buscarSerie() {
-        let buscar = prompt('Ingrese el nombre de la serie que quiere ver: ').toUpperCase()
-        let resultSerie = series.includes(buscar)
-            if (resultSerie === false){
-             ('Nuestro catalogo no cuenta con la series: ', buscar)
-            } else {
-            console.log('En breve se reproducira la serie: ', buscar)
-            }
-    }
-
-    function eliminarSerie (){
-        let sacarSerie = prompt('Ingrese el titulo de la serie a agregar: ').toUpperCase()
-        let lugarSerie = series.indexOf(sacarSerie)
-        if (lugarSerie === -1){
-            console.warn('Serie no eliminada, no esta en nuestro listado')
-        } else {
-            series.splice(lugarSerie, 1)
-            console.log('Se elimino la serie: ', sacarSerie)
-        }
-    }
-
-    function limpiarHTMLPeli(){
-        seccionPeli.innerHTML = '';
-    }
-
-    
-
-    //Eventos
-
-    btnBuscarPeli.addEventListener('click', () => {
-        const textPeli = nombreBuscarPeli.value
-        peliDisponible(textPeli)})
-
-    btnAgregarPeli.addEventListener('click', ()=>{
-        const nombreAgregado = inpNombre.value.toLowerCase()
-        const generoAgregado = inpGenero.value
-        const califAgregado = inpCalif.value
-        agregarPeli(nombreAgregado, generoAgregado, califAgregado)
-    })
+miListaHTML()
+crearPelis()
